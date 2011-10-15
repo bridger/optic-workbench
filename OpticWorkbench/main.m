a77fe4817c2b7b3414573bbe0bc0c1c3144f9383
Bridger Maxwell <bridgeyman@gmail.com>
2011-10-15 15:28:51 -0400

    Initial Commit

---
+++ OpticWorkbench/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  OpticWorkbench
+//
+//  Created by Bridger Maxwell on 10/15/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
